06a785f693aec071ffe35a590ca71b6980e63b13
sakthivel <sakthiveltofficial@gmail.com>
2025-07-22 11:22:14 +0530

    patch

--- src/components/CanvesWrapper.jsx
+++ src/components/CanvesWrapper.jsx
@@ -10,7 +10,7 @@ import React, { useEffect, useRef } from "react";
 import BaseEnvironment from "./BaseEnvironment";
 import { EffectComposer, Noise } from "@react-three/postprocessing";
 
-function CanvesWrapper({ children, isActive = false }) {
+function CanvesWrapper({ children }) {
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
@@ -19,44 +19,44 @@ function CanvesWrapper({ children, isActive = false }) {
   }, []);
 
   return (
-    <div className="w-full h-full relative">
-      {/* F2F2F2 */}
-      <Canvas
-        camera={{ fov: 40, position: [0, 0, 20] }}
-        gl={{ antialias: true, preserveDrawingBuffer: true }}
-        dpr={[1, 1.5]}
-        shadows
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "110%",
-          height: "100%",
-          transform: "translateX(-2%)",
-        }}
-      >
-        <BaseEnvironment />
-
-        {children}
-        {/* <OrbitControls /> */}
-        <ambientLight intensity={0.5} />
-        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-          <GizmoViewport
-            axisColors={["red", "green", "blue"]}
-            labelColor="black"
-          />
-        </GizmoHelper>
-        {/* <Grid
+    <div className="w-full h-full relative p-5">
+      <div className="w-full h-full relative bg-[#F2F2F2] rounded-[3rem] overflow-hidden">
+        <Canvas
+          camera={{ fov: 40, position: [0, 0, 20] }}
+          gl={{ antialias: true, preserveDrawingBuffer: true }}
+          dpr={[1, 1.5]}
+          shadows
+          style={{
+            position: "absolute",
+            top: 0, 
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          
+          <BaseEnvironment />
+          {children}
+          <OrbitControls  />
+          <ambientLight intensity={0.5} />
+          <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+            <GizmoViewport
+              axisColors={["red", "green", "blue"]}
+              labelColor="black"
+            />
+          </GizmoHelper>
+          {/* <Grid
             position={[0, -0.65, 0]}
             args={[150, 200]}
             cellColor="black"
             cellSize={1}
             cellThickness={1}
           /> */}
-        {/* <EffectComposer>
-              <Noise opacity={0.001} />
-            </EffectComposer> */}
-      </Canvas>
+          <EffectComposer>
+            <Noise opacity={0.01} />
+          </EffectComposer>
+        </Canvas>
+      </div>
     </div>
   );
 }
