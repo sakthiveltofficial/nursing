6de4fe04d826ecefacb2412bd87e3db3ef32a612
sakthivel <sakthiveltofficial@gmail.com>
2025-08-01 10:36:01 +0530

    font has been changed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,11 +3,17 @@ import "./globals.css";
 import ToggleMenu from "@/components/Common/Header";
 import ScrollToTop from "@/components/Common/ScrollToTop";
 import Footer from "@/components/Footer";
+import { Readex_Pro } from 'next/font/google';
+
+const readexPro = Readex_Pro({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased overflow-x-hidden`}>
+      <body className={`${readexPro.className} antialiased overflow-x-hidden`}>
       <ToggleMenu/>
         {children}
         <Footer/>
